4407b4b5b06616ab91eafa199e8b1068f600401f
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:10:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,15 +3,14 @@ var playlist = {
   "Phil Ochs":"Here's to the State of Mississippi"};
   return playlist;
 
-var playlist = {
-  "Slowdive": "Alison",
-  "My Bloody Valentine":"Sometimes"
-};
+//var playlist = {
+  //"Slowdive": "Alison",
+ // "My Bloody Valentine":"Sometimes"
+//};
 
 function updatePlaylist(playlist, ArtistName, SongTitle ){
  return songTitle;
 }
-function updatePlaylist = {playlist, artistName, songTitle:"songTitle"
 
 function removeFromPlaylist (playlist, artistName){
   delete playlist[artistName];
