c1437582b870f54a32a97ef87cc09aa5bc9857a9
Suheyl Turac <skbsturac@live.com>
2018-08-31 22:54:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,12 @@
 var playlist = {playlist};
 var updatePlaylist = {playlist, artistName, songTitle: "songTitle"};
+var playlist= {
+  "Slowdive": "Alison",
+  "My Bloody Valentine":"Sometimes"
+};
+
+
+function updatePlaylist(playlist, ArtistName, SongTitle ){
+ return playlist.
+}
 Function removeFromPLaylist (playlist, artistName)
