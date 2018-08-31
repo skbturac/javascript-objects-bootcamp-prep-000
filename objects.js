36c8f93d438e8003c70dc72d6cc5c545a7d06730
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:13:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,8 @@
 
 var playlist = {
   "Slowdive": "Alison",
-  "My Bloody Valentine":"Sometimes"
+  "My Bloody Valentine":"Sometimes",
+  "Phil Ochs": "Here's to the State of Mississippi"
 };
 
 function updatePlaylist(playlist, artistName, songTitle ){
