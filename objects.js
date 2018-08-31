25452b60bd11f032b99cd11deb0fa469294b5edc
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:11:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,10 +3,10 @@
   //"Phil Ochs":"Here's to the State of Mississippi"};
   //return playlist;
 
-//var playlist = {
+var playlist = {
   //"Slowdive": "Alison",
  // "My Bloody Valentine":"Sometimes"
-//};
+};
 
 function updatePlaylist(playlist, ArtistName, SongTitle ){
  return songTitle;
