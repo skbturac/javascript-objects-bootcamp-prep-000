6d3aa92c1b79287043bbd5575c491348103ba69a
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:03:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,11 @@ var playlist = {
 var updatePlaylist = {playlist, artistName, songTitle: "songTitle"
 };
 
+function removeFromPlaylist(playlist, artistName){
+  var playlist = { 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"};
+  return playlist
+}
+
 function removeFromPlaylist (playlist, artistName){
 delete playlist[artistName];
 }
