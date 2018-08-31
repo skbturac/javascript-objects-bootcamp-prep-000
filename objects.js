2eb31cd6ad4422405c4e71dfaf7085e2d315a66c
Suheyl Turac <skbsturac@live.com>
2018-08-31 23:04:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,11 @@
 var playlist = {
-  "My Bloody Valentine": "Bana Sen Lazimsin",
-  "Ahmet Kaya":"Kum Gibi"
-};
+  "My Bloody Valentine": "Sometimes",
+  "Phil Ochs":"Here's to the State of Mississippi"};
+  return playlist;
 
 var updatePlaylist = {playlist, artistName, songTitle: "songTitle"
-};
 
 function removeFromPlaylist(playlist, artistName){
-  var playlist = { 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"};
   return playlist
 }
 
